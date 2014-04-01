c45b125d8235c2e36bd812f7b99ed59eaa1ad3cd
Mehmet Beydogan <mbbrain@gmail.com>
2014-04-01 14:32:04 +0300

    -updates partials and views -adds custom css file

--- app/js/app.js
+++ app/js/app.js
@@ -10,7 +10,7 @@ angular.module('blogularApp', [
   'blogularApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/view1', {templateUrl: 'partials/home.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
